4555dda85358698f046c401e834ec6fc4ab3ac3c
J3sus-esb <jealpartidavi@ittepic.edu.mx>
2026-04-28 23:02:37 -0700

    Eliminar carpeta anidada duplicada

--- api_res_invertions/src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-
-const port = app.get('port') || process.env.PORT || 3000;
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`API listening on port ${port}`);
-});
